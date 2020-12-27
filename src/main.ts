2a68f9bda4db0d534681363b70fc0eb9b64ebe0f
詹志强 <zqzhan@kaikeba.com>
2020-12-28 00:15:53 +0800

    feat: 升级基础库

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import getLogger from './logger/morgan';
 import { GlobalErrorFilterMiddleware } from './middleware/GlobalErrorFilter.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(getLogger());
   app.useGlobalFilters(new GlobalErrorFilterMiddleware());
   await app.listen(3000);
 }
